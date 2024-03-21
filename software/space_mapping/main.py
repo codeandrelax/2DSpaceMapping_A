9362a5767301edfa9b4b39e680af0ce9d23b6b76
Luka Vidic <luka.vidic01@gmail.com>
2024-03-21 16:25:45 +0100

    Issue #15 : Implementirati upravljanje kretanja kroz aplikaciju
    - Dodan mehanizam za START/STOP kontrolu kretanja
    - Implementiran način komunikacije datih događaja
    - Male izmjene na GUI ekranu

--- software/space_mapping/main.py
+++ software/space_mapping/main.py
@@ -1,15 +1,30 @@
 import asyncio
 from websockets.server import serve
+from websockets.sync.client import connect
 import pygame, math, numpy
 
-ROBOT_RADIUS = 80
+ROBOT_RADIUS = 20
+START_RANGE_X = [0,100]
+STOP_RANGE_X = [700,800]
+RANGE_Y = [760,800]
+ROBOT_IP_ADDR = "192.168.100.75"
 
 lidar_data = []
+
+def draw_start_and_stop(screen, font):
+    text_start = font.render('START', True, pygame.Color(0,0,0))
+    text_stop = font.render('STOP', True, pygame.Color(0,0,0))
+    pygame.draw.rect(screen, pygame.Color(0,255,0),[0,760,100,40])
+    pygame.draw.rect(screen, pygame.Color(255,0,0),[700,760,100,40])
+    screen.blit(text_start, (15,770)) 
+    screen.blit(text_stop, (720,770))
 
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 sysfont = pygame.font.get_default_font()
-font1 = pygame.font.SysFont(sysfont, 72)
+font = pygame.font.SysFont(sysfont, 32)
+draw_start_and_stop(screen, font)
+pygame.display.flip()
 
 # Function that converts LIDAR data given in distance and angle 
 # to the Cartesian coordinates of the obstacle in the space.
@@ -26,6 +41,7 @@ async def receiver(websocket):
             lidar_data = data_temp.astype(float)
         #print("--------------------------------------------------------------------------------------")
         #print(lidar_data)
+        await check_start_stop()
         await screen_update()
 
 async def screen_update():
@@ -39,7 +55,22 @@ async def screen_update():
         pygame.draw.circle(screen, pygame.Color(0,0,0), point, 4)
     pygame.draw.circle(screen, pygame.Color(0, 0, 200), (400, 400), ROBOT_RADIUS)
     pygame.draw.circle(screen, pygame.Color(0,0,0), (400,400), ROBOT_RADIUS, width=2)
+    draw_start_and_stop(screen, font)
     pygame.display.flip()
+
+async def check_start_stop():
+    mouse_pos = pygame.mouse.get_pos()
+    for ev in pygame.event.get(): 
+        if ev.type == pygame.MOUSEBUTTONDOWN: 
+            #print("Mouse position:", mouse_pos)
+            if (START_RANGE_X[0] <= mouse_pos[0] <= START_RANGE_X[1]) and (RANGE_Y[0] <= mouse_pos[1] <= RANGE_Y[1]):
+                print("USER PRESSED THE START BUTTON")
+                #with connect("ws://" + ROBOT_IP_ADDR + "8765") as websocket:
+                #    websocket.send("1")
+            if (STOP_RANGE_X[0] <= mouse_pos[0] <= STOP_RANGE_X[1]) and (RANGE_Y[0] <= mouse_pos[1] <= RANGE_Y[1]):
+                print("USER PRESSED THE STOP BUTTON")
+                #with connect("ws://" + ROBOT_IP_ADDR + "8765") as websocket:
+                #    websocket.send("0")
 
 async def main():
     async with serve(receiver, "0.0.0.0", 8765):
