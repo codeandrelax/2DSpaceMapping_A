5ce3ddeadf8cfc25ad8371db8a48d04fa0c77c25
Luka Vidic <luka.vidic01@gmail.com>
2024-04-04 23:29:28 +0200

    Issue #28 : Modifikacija aplikacije za mapiranje prostora
    - Izmjenjena aplikacija tako da se prilagodi podacima koji dolaze sa stvarnog sistema
    - Zadržane funkcionalnosti i prilikom rada sa simulatorom

--- software/space_mapping/main.py
+++ software/space_mapping/main.py
@@ -9,6 +9,9 @@ STOP_RANGE_X = [700,800]
 RANGE_Y = [760,800]
 ROBOT_IP_ADDR = "192.168.100.75"
 
+# ASSERT IF SIMULATOR IS USED
+SIMULATOR = 0
+
 lidar_data = []
 
 def draw_start_and_stop(screen, font):
@@ -33,10 +36,44 @@ def da_to_pos(distance, angle, robot_pos):
     obstacle_y = -distance * numpy.sin(angle) + robot_pos[1]
     return(int(obstacle_x), int(obstacle_y))
 
+def conv_voltage_to_distance(voltage):
+    distance = 0.0
+    if(voltage > 1.9):
+        distance = 10.0
+    elif(voltage <= 1.9 and voltage > 1.02):
+        distance = -11.36 * voltage + 31.59
+    elif(voltage <= 1.02 and voltage > 0.8):
+        distance = -45.45 * voltage + 66.36
+    elif(voltage <= 0.8 and voltage > 0.55):
+        distance = -80.0 * voltage + 94.0
+    elif (voltage <= 0.55 and voltage > 0.35):
+        distance = -500.0 * voltage + 325.0
+    else:
+        distance = -1.0
+    return distance
+
+def get_angle_from_duty_cycle(duty_cycle):
+    angle_deg = (duty_cycle - 2.5) * 18.0
+    angle_rad = angle_deg * math.pi / 180.0
+    return angle_rad
+
+def convert_data(data):
+    conv_data = []
+    for i in range(len(data)//2):
+        distance = conv_voltage_to_distance(data[2*i])
+        angle = get_angle_from_duty_cycle(data[2*i+1])
+        if distance > 0.0:
+            conv_data.append(distance)
+            conv_data.append(angle)
+    return conv_data
+
+
 async def receiver(websocket):
     global lidar_data
     async for message in websocket:
         data_temp = numpy.asarray(message.split(","))
+        if SIMULATOR == 0:
+            data_temp = convert_data(data_temp)
         if len(data_temp) > 1:
             lidar_data = data_temp.astype(float)
         #print("--------------------------------------------------------------------------------------")
