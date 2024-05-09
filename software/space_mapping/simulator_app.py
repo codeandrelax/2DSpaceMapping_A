5ead00bfe06542b96dca7d2d6364cd5e939bb345
lukavidic <luka.vidic01@gmail.com>
2024-05-09 10:48:41 +0200

    Issue #31 : Modifikacija softverskih aplikacija
    - Uklonjeno još nepotrebnih funkcionalnosti iz simulator aplikacije

--- software/space_mapping/simulator_app.py
+++ software/space_mapping/simulator_app.py
@@ -16,16 +16,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,800))
 sysfont = pygame.font.get_default_font()
 font = pygame.font.SysFont(sysfont, 32)
-draw_start_and_stop(screen, font)
 pygame.display.flip()
-
-def draw_start_and_stop(screen, font):
-    text_start = font.render('START', True, pygame.Color(0,0,0))
-    text_stop = font.render('STOP', True, pygame.Color(0,0,0))
-    pygame.draw.rect(screen, pygame.Color(0,255,0),[0,760,100,40])
-    pygame.draw.rect(screen, pygame.Color(255,0,0),[700,760,100,40])
-    screen.blit(text_start, (15,770)) 
-    screen.blit(text_stop, (720,770))
 
 # Function that converts LIDAR data given in distance and angle 
 # to the Cartesian coordinates of the obstacle in the space.
@@ -43,7 +34,6 @@ async def receiver(websocket):
             lidar_data = data_temp.astype(float)
         #print("--------------------------------------------------------------------------------------")
         #print(lidar_data)
-        await check_start_stop()
         await screen_update()
 
 async def screen_update():
@@ -57,22 +47,7 @@ async def screen_update():
         pygame.draw.circle(screen, pygame.Color(0,0,0), point, 4)
     pygame.draw.circle(screen, pygame.Color(0, 0, 200), (400, 400), ROBOT_RADIUS)
     pygame.draw.circle(screen, pygame.Color(0,0,0), (400,400), ROBOT_RADIUS, width=2)
-    draw_start_and_stop(screen, font)
     pygame.display.flip()
-
-async def check_start_stop():
-    mouse_pos = pygame.mouse.get_pos()
-    for ev in pygame.event.get(): 
-        if ev.type == pygame.MOUSEBUTTONDOWN: 
-            #print("Mouse position:", mouse_pos)
-            if (START_RANGE_X[0] <= mouse_pos[0] <= START_RANGE_X[1]) and (RANGE_Y[0] <= mouse_pos[1] <= RANGE_Y[1]):
-                print("USER PRESSED THE START BUTTON")
-                #with connect("ws://" + ROBOT_IP_ADDR + str(PORT)) as websocket:
-                #    websocket.send("1")
-            if (STOP_RANGE_X[0] <= mouse_pos[0] <= STOP_RANGE_X[1]) and (RANGE_Y[0] <= mouse_pos[1] <= RANGE_Y[1]):
-                print("USER PRESSED THE STOP BUTTON")
-                #with connect("ws://" + ROBOT_IP_ADDR + str(PORT)) as websocket:
-                #    websocket.send("0")
 
 async def main():
     async with serve(receiver, "0.0.0.0", PORT):
